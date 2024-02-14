93613e4fc2ba6614571a793798892a1d8f65ae54
edevars <e.devars.dev@gmail.com>
2024-02-14 01:17:09 -0600

    feat: updating suscriptor

--- newsletter/api/serializers.py
+++ newsletter/api/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from newsletter.models import Suscriptor
 
@@ -5,8 +6,19 @@ class SuscriptorSerializer(ModelSerializer):
   class Meta:
     model = Suscriptor
     fields = ['email', 'name','suscribed']
+    read_only_fields = ['email']
 
 class CreateSuscriptorSerializer(ModelSerializer):
   class Meta:
     model = Suscriptor
     fields = ['email', 'name']
+
+class UnsuscribeSerializer(serializers.Serializer):
+  email = serializers.EmailField()
+  suscribed = serializers.BooleanField()
+
+  def update(self, instance, validated_data):
+    instance.email = validated_data.get('email', instance.email)
+    instance.suscribed = validated_data.get('suscribed', instance.suscribed)
+    instance.save()
+    return instance
--- newsletter/api/views.py
+++ newsletter/api/views.py
@@ -2,12 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from newsletter.models import Suscriptor
-from newsletter.api.serializers import SuscriptorSerializer, CreateSuscriptorSerializer
+from newsletter.api.serializers import SuscriptorSerializer, CreateSuscriptorSerializer, UnsuscribeSerializer
 
 
 class SuscriptorApiView(APIView):
 
-  
   def get(self, request):
     suscriptors = Suscriptor.objects.all()
     serialized_suscriptors = SuscriptorSerializer(suscriptors, many=True)
@@ -17,9 +16,23 @@ class SuscriptorApiView(APIView):
     )
 
 
-  def post(self, request, *args, **kwargs):
+  def post(self, request):
     serializer = CreateSuscriptorSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  def put(self, request, *args, **kwargs):
+    email = request.data['email']
+    
+    try:
+      suscriptor = Suscriptor.objects.get(email=email)
+    except Suscriptor.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = UnsuscribeSerializer(suscriptor, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
