6cfc243b6aa125059728e2d922ee9a7273eb51bf
edevars <e.devars.dev@gmail.com>
2024-02-18 20:16:56 -0600

    feat: transforming template content to html

--- newsletter/api/email_utils.py
+++ newsletter/api/email_utils.py
@@ -1,6 +1,14 @@
 from newsletter.models import Template, Suscriptor
 from django.core.mail import EmailMessage, get_connection
 from django.conf import settings
+import markdown
+
+def transform_content_to_html(original_content):
+  content = markdown.markdown(original_content)
+  return content
+
+def custome_body(body,name=""):
+  return body.replace("{{name}}",name)
 
 def send_email(template_id, recipients):
   try:
@@ -10,24 +18,29 @@ def send_email(template_id, recipients):
       suscriptors = Suscriptor.objects.filter(suscribed=True)
     
     subject = template.subject
-    content = template.content
+    content = transform_content_to_html(template.content)
     file_path = template.attached_file.path
     success_emails = []
     messages = []
 
     for suscriptor in suscriptors:
       email = ""
+      name= ""
       if len(recipients) > 0:
         email = suscriptor["email"]
       else:
         email = suscriptor.email
+        name = suscriptor.name
       
+      custome_content = custome_body(content,name)
+
       message = EmailMessage(
         subject=subject,
-        body=content,
+        body=custome_content,
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=[email],
       )
+      message.content_subtype = 'html'
 
       if file_path:
         message.attach_file(file_path)
